b15e07a33f0105dd740034391f39159951246a23
ztoufik <toufikouedtlelat@gmail.com>
2022-12-25 17:21:44 +0100

    scalar_poly_expanser class + testsuite

--- main.py
+++ main.py
@@ -1,75 +1,48 @@
 import chaospy as cp
 import numpy as np
-#model
-'''
-def model(x,u0,c0,c1,c2):
-    def c(x):
-        if x<0.5: return c0
-        elif 0.5<=x<0.7: return c1
-        else:  return c2
 
-    N=len(x)
-    u=np.zeros(N)
+class Scalar_Poly_Expanser:
+    def __init__(self, number_joint_RV,poly_order,quadrature_intg_order):
+        self.number_joint_RV=number_joint_RV#(iid RV) number of parameters model to evaluate accepts 
+        self.poly_order=poly_order
+        self.quadrature_intg_order=quadrature_intg_order
+        self.normal_dist=cp.Normal(0,1)
+        self.iid_normal_dist=cp.Iid(self.normal_dist,self.number_joint_RV)
 
-    u[0]=u0
-    for n in range(N-1):
-        dx=x[n+1]-x[n]
-        K1=-dx*u[n]*c(x[n])
-        K2=-dx*u[n]+K1/2*c(x[n]+dx/2)
-        u[n+1]=u[n]+K1+K2
-    return u
-'''
-model=lambda arr:arr[1]
+    def generate_quad_nodes_weights(self,tolerance=1e-5,recurrence_algorithm="stieltjes",scaling=5):
+        self.nodes,self.weights=cp.generate_quadrature(self.quadrature_intg_order, self.iid_normal_dist, rule="gaussian",
+                                         recurrence_algorithm=recurrence_algorithm ,tolerance=tolerance,scaling=scaling)
 
-#parameters
-normal_dist=cp.Normal(0.0,1.0)
-number_joint_RV=3#memory delay parameters
-quadrature_intg_order=10
-hermite_order=5#polynomial orders
-joint_normal_dist=cp.Iid(normal_dist,number_joint_RV)
+    def generate_poly_expansion(self):
+        self.expansions = cp.generate_expansion(self.poly_order, self.iid_normal_dist,normed=True,reverse=True)
 
-#generate qudrature nodes
-print("generate qudrature nodes")
-nodes,weights=cp.generate_quadrature(quadrature_intg_order, joint_normal_dist, rule="gaussian")
-nodes=nodes.round(3)
-weights=weights.round(3)
-print("nodes",nodes)
-print("weights",weights)
-#print("len(nodes[0])",len(nodes[0]))
-#Evaluating model 
-print("Evaluating model")
-evals=[model(node) for node in nodes.T]
+    def estimate_fourier_coefs(self,model):
+        self.model_evals=np.array([model(node) for node in self.nodes.T])
+        self.poly_expansion,self.fourier_coefs,self.poly_evals=cp.fit_quadrature(self.expansions, self.nodes, self.weights, self.model_evals,retall=2)
 
-#polynomial expansion
-print("polynomial expansion")
-expansions = cp.generate_expansion(hermite_order, joint_normal_dist).round(3)
-print("expansions:",expansions)
+if __name__=="__main__":
+    number_joint_RV=3
+    poly_order=5
+    quadrature_intg_order=5
+    tolerance=1e10-5
+    quadrature_algo="stieltjes"
+    scaling=5
 
-#Fourier coefficients
-print("Fourier coefficients")
-model_approx,coefs=cp.fit_quadrature(expansions, nodes, weights, evals,retall=True)
-print("coefs:",coefs)
-approx_evals = model_approx(*nodes).round(3)
+    models={}
+    models["tanh"]=lambda arr:np.tanh(arr[0])
+    models["linear"]=lambda arr:0.34*arr[0]
+    models["delay1"]=lambda arr:arr[1]
+    models["delay2"]=lambda arr:arr[2]
+    poly_expanser=Scalar_Poly_Expanser(number_joint_RV,poly_order,quadrature_intg_order)
+    print("generate nodes & weights")
+    poly_expanser.generate_quad_nodes_weights(tolerance,quadrature_algo,scaling)
+    print("generate poly expansion")
+    poly_expanser.generate_poly_expansion()
+    error={}
+    for model in models.keys():
+        poly_expanser.estimate_fourier_coefs(models[model])
+        error_vector=(poly_expanser.model_evals-poly_expanser.poly_evals.T.dot(poly_expanser.fourier_coefs))/poly_expanser.model_evals
+        error[model]=np.mean(np.abs(error_vector)).round(3)
 
-#evaluations
-print("appxorimation",approx_evals)
-print("model",evals)
-
-'''
-
-
-
-
-hermite_polys=cp.generate_expansion(hermite_order, joint_normal_dist).round(5)
-print(hermite_polys)
-nodes,weights=cp.generate_quadrature(order=quadrature_intg_order,dist=joint_normal_dist,rule="Gaussian")
-model_evals=np.array([model(node,0.3,0.5,0.5,0.03) for node in nodes.T])
-fourier_coef=cp.fit_quadrature(hermite_polys,nodes,weights,model_evals)
-print(fourier_coef)
-x=np.linspace(0,1,100)
-true_evals=model(x,0.3,0.5,0.5,0.03) 
-approx_evals=fourier_coef(*x)
-print("true evals:",true_evals)
-print("approx evals:",approx_evals)
-#print(np.linalg.norm(approx_evals-true_evals))
-'''
+    for model in error:
+        print(model,error[model])
