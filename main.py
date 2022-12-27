ab82b95ffbccd1c13555e1c135feb09292c864d8
ztoufik <toufikouedtlelat@gmail.com>
2022-12-27 08:16:28 +0100

    separate model evaluation from scalar_poly_expanser

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ class Scalar_Poly_Expanser:
         self.number_joint_RV=number_joint_RV#(iid RV) number of parameters model to evaluate accepts 
         self.poly_order=poly_order
         self.quadrature_intg_order=quadrature_intg_order
-        self.iid_joint_dist=cp.Iid(cp.Uniform(0,1),self.number_joint_RV)# Hash don't change if you change the distribution
-        self.nodes_file_name=f"nodes{hash((self.quadrature_intg_order,self.number_joint_RV))}.pi"
-        self.expansions_file_name=f"expansions{hash((self.poly_order,self.number_joint_RV))}.pi"
-
+        self.iid_joint_dist=cp.Iid(cp.Uniform(0,1),self.number_joint_RV)# (Remove the serialized cache if you changed dist (Hash don't change if you change the distribution)
+        self.nodes_hash=hash((self.quadrature_intg_order,self.number_joint_RV))
+        self.expansions_hash=hash((self.poly_order,self.number_joint_RV))
+        self.nodes_file_name=f"nodes{self.nodes_hash}.pi"
+        self.expansions_file_name=f"expansions{self.expansions_hash}.pi"
 
     def generate_quad_nodes_weights(self):
         if os.path.exists(self.nodes_file_name):
-            #loading
             self.nodes,self.weights=utils.load_data(self.nodes_file_name)
         else:
              self.nodes,self.weights=cp.generate_quadrature(self.quadrature_intg_order, self.iid_joint_dist, rule="gaussian",
@@ -24,15 +24,14 @@ class Scalar_Poly_Expanser:
 
     def generate_poly_expansion(self):
         if os.path.exists(self.expansions_file_name):
-            #loading
             self.expansions=utils.load_data(self.expansions_file_name)
         else:
              self.expansions = cp.generate_expansion(self.poly_order, self.iid_joint_dist,normed=True,reverse=True)
              utils.save_data(self.expansions_file_name,self.expansions)
 
-    def estimate_fourier_coefs(self,model):
-        self.model_evals=np.array([model(node) for node in self.nodes.T])
-        self.poly_expansion,self.fourier_coefs,self.poly_evals=cp.fit_quadrature(self.expansions, self.nodes, self.weights, self.model_evals,retall=2)
+    def estimate_fourier_coefs(self,model_evals):
+        self.poly_expansion,self.fourier_coefs,self.poly_evals=cp.fit_quadrature(
+                self.expansions, self.nodes, self.weights, model_evals,retall=2)
 
 if __name__=="__main__":
     number_joint_RV=3
@@ -56,7 +55,8 @@ if __name__=="__main__":
     evaluation_nodes=np.random.rand(10,number_joint_RV)
     error={}
     for model in models.keys():
-        poly_expanser.estimate_fourier_coefs(models[model])
+        model_evals=[models[model](node) for node in poly_expanser.nodes.T]
+        poly_expanser.estimate_fourier_coefs(np.array(model_evals))
         poly_model_evals=poly_expanser.poly_expansion(*evaluation_nodes.T)
         #print("poly model:",poly_model_evals)
         model_evals=np.array([models[model](node) for node in evaluation_nodes])
